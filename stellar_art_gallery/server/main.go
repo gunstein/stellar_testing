d49c23359322de9e0ed581385c95696a9f4c6ad6
Gunstein Vatnar <gunstein.vatnar@gmail.com>
2021-01-19 22:09:08 +0100

    working version, dockerizing is next step

--- stellar_art_gallery/server/main.go
+++ stellar_art_gallery/server/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"net/smtp"
+		
 	"strconv"
 	"strings"
 
@@ -23,15 +24,18 @@ func SendEmailSMTP(emailTo string, emailFrom string, emailHost string, emailPass
 
 	emailAuth := smtp.PlainAuth("", emailFrom, emailPassword, emailHost)
 
-	emailBody := "TODO: build a useful message with url"
+	emailBody := fmt.Sprintf(`<html><body><h1>Downloadlink from StellarGallery</h1>
+	<a href="%s">%s</a>
+	</body></html>`, big_image_url, big_image_url)
 
-	mime := "MIME-version: 1.0;\nContent-Type: text/plain; charset=\"UTF-8\";\n\n"
-	subject := "Subject: " + "Test Email" + "!\n"
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	subject := "Subject: " + "Download your purchased art" + "!\n"
 	msg := []byte(subject + mime + "\n" + emailBody)
 	addr := fmt.Sprintf("%s:%s", emailHost, emailPort)
 
 	emailToArray := []string{emailTo}
 	if err := smtp.SendMail(addr, emailAuth, emailFrom, emailToArray, msg); err != nil {
+		fmt.Println(err)
 		return false, err
 	}
 	return true, nil
@@ -43,20 +47,37 @@ func main() {
 	emailHost := flag.String("email_host", "not_set_email_host", "email host")
 	emailPassword := flag.String("email_password", "not_set_email_password", "email password")
 	emailPort := flag.String("email_port", "not_set_email_port", "email port")
-
+	flag.Parse();
 	// Build and connect to database
 	models.ConnectDatabase()
 
 	client := horizonclient.DefaultTestNetClient //DefaultPublicNetClient
 	// payments for an account
+	fmt.Println(*account_publickey)
 	opRequest := horizonclient.OperationRequest{ForAccount: *account_publickey}
+	//opRequest := horizonclient.OperationRequest{ForAccount: "GBGJFGCDZHQ3LXJOUK7EOZB77OR2GMES3FVQRK4M724THUDLZLP7J6A7"}
+	//opRequest := horizonclient.OperationRequest{Cursor: "760209215489"}
 	ctx := context.Background()
+
 	paymentHandler := func(op operations.Operation) {
+		fmt.Println("Payment received.")
+
 		transaction, err := client.TransactionDetail(op.GetTransactionHash())
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
+		//confirm type is payment
+		payment, ok := op.(operations.Payment)
+		if !ok {
+			fmt.Println("Not payment type operation")
+			return
+		}
+		if strings.EqualFold(payment.From, *account_publickey) {
+			//Not interested in outgoing payments
+			return
+		}
+		//Get orderid from memo		
 		memo := transaction.Memo
 		u64, err := strconv.ParseUint(memo, 10, 32)
 		if err != nil {
@@ -68,22 +89,15 @@ func main() {
 			fmt.Println(err)
 			return
 		}
+		//get art from orderid
 		art, err := models.GetArtForId(order.ArtId)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		//Check payment amount
-		payment, ok := op.(operations.Payment)
-		if !ok {
-			fmt.Println("Not payment type operation")
-			return
-		}
-		if strings.EqualFold(payment.From, *account_publickey) {
-			//Not interested in outgoing payments
-			return
-		}
 
+
+		//Check payment amount
 		value, err := strconv.ParseFloat(payment.Amount, 32)
 		if err != nil {
 			fmt.Println("Conversion to float failed.")
@@ -94,15 +108,37 @@ func main() {
 			fmt.Println("Paid to little.")
 			return
 		}
+		//update order to paid
+		order, err = models.UpdateOrderToPaid(order.ID)
+		if err != nil {
+			fmt.Println("UpdateOrderToPaid failed.")
+			return
+		}
 
-		SendEmailSMTP(order.Email, *emailFrom, *emailHost, *emailPassword, *emailPort, art.BigFileUrl)
+		sent, err := SendEmailSMTP(order.Email, *emailFrom, *emailHost, *emailPassword, *emailPort, art.BigFileUrl)
+		if !sent || err != nil {
+			fmt.Println("SendEmailSMTP failed.")
+			return
+		}
+
+		//update order to emailsent
+		order, err = models.UpdateOrderToEmailSent(order.ID)
+		if err != nil {
+			fmt.Println("UpdateOrderToEmailSent failed.")
+			return
+		}	
+		
 	}
 	fmt.Println("Before streaming.")
-	err := client.StreamPayments(ctx, opRequest, paymentHandler)
-	if err != nil {
-		fmt.Println(err)
-	}
+	go func() {
+		err := client.StreamPayments(ctx, opRequest, paymentHandler)
+		if err != nil {
+			fmt.Println(err)
+		}
+    }()
+
 	fmt.Println("After streaming.")
+
 	r := gin.Default()
 	corsConfig := cors.DefaultConfig()
 	corsConfig.AllowOrigins = []string{"*"}
--- stellar_art_gallery/server/models/models.go
+++ stellar_art_gallery/server/models/models.go
@@ -9,20 +9,21 @@ import (
 
 type Art struct {
 	gorm.Model
-	Title        string
+	Title        string `gorm:"not null"`
 	Description  string
-	Artist       string
-	Price        float32
-	SmallFileUrl string
-	BigFileUrl   string
+	Artist       string 
+	Price        float32 `gorm:"not null"`
+	SmallFileUrl string `gorm:"not null"`
+	BigFileUrl   string `gorm:"not null"`
 	Order        []Order
 }
 
 type Order struct {
 	gorm.Model
-	Email string
-	Paid  bool
-	ArtId uint
+	Email string `gorm:"not null"`
+	Paid  bool `gorm:"default:false"`
+	EmailSent bool `gorm:"default:false"`
+	ArtId uint `gorm:"not null"`
 }
 
 var DB *gorm.DB
@@ -36,15 +37,20 @@ func ConnectDatabase() {
 
 	db.AutoMigrate(&Art{}, &Order{})
 
-	art1 := Art{Title: "Sushi", Description: "Made in 2020 during the Lockdown.", Artist: "Anneli", Price: 1, SmallFileUrl: "https://drive.google.com/uc?export=view&id=1eU1DLGWLha8KfJct5DWS2WadJz0RcifC", BigFileUrl: "big_aurora_borealis.jpg"}
+	art1 := Art{Title: "Sushi", Description: "Made in 2020 during the Lockdown.", Artist: "Anneli", Price: 1, SmallFileUrl: "https://drive.google.com/uc?export=view&id=1eU1DLGWLha8KfJct5DWS2WadJz0RcifC", BigFileUrl: "https://drive.google.com/uc?export=view&id=1kZFnq6rwmKjJ0P8B9ClEWXcdOlkBz98s"}
 	db.Create(&art1)
-	art2 := Art{Title: "ScoobyDoo", Description: "Made in 2020 during the Lockdown.", Artist: "Anneli", Price: 1, SmallFileUrl: "https://drive.google.com/uc?export=view&id=1TciUqcac_FN1P-c-h1gnhYlnT2cnK1qa", BigFileUrl: "big_aurora_borealis.jpg"}
+	art2 := Art{Title: "ScoobyDoo", Description: "Made in 2020 during the Lockdown.", Artist: "Anneli", Price: 1, SmallFileUrl: "https://drive.google.com/uc?export=view&id=1TciUqcac_FN1P-c-h1gnhYlnT2cnK1qa", BigFileUrl: "https://drive.google.com/uc?export=view&id=1hKwCJ-x3UKwJEfS-HRRPL9OkgMPv5YSn"}
 	db.Create(&art2)
-	art3 := Art{Title: "Paperbag", Description: "Made in 2020 during the Lockdown.", Artist: "Anneli", Price: 1, SmallFileUrl: "https://drive.google.com/uc?export=view&id=1IxDAhNTq5V7TTRE3s0xfwvvb0GaLk3Fh", BigFileUrl: "big_aurora_borealis.jpg"}
+	art3 := Art{Title: "Paperbag", Description: "Made in 2020 during the Lockdown.", Artist: "Anneli", Price: 1, SmallFileUrl: "https://drive.google.com/uc?export=view&id=1IxDAhNTq5V7TTRE3s0xfwvvb0GaLk3Fh", BigFileUrl: "https://drive.google.com/uc?export=view&id=1a4U3D_znz-_vRccPCB_yRw5XGRSWEoQH"}
 	db.Create(&art3)
-	art4 := Art{Title: "LionKing", Description: "Made in 2020 during the Lockdown.", Artist: "Anneli", Price: 1, SmallFileUrl: "https://drive.google.com/uc?export=view&id=1VgD7J5lYPoDLtJ6d9-sJH4b9u6eCyGXD", BigFileUrl: "big_aurora_borealis.jpg"}
+	art4 := Art{Title: "LionKing", Description: "Made in 2020 during the Lockdown.", Artist: "Anneli", Price: 1, SmallFileUrl: "https://drive.google.com/uc?export=view&id=1VgD7J5lYPoDLtJ6d9-sJH4b9u6eCyGXD", BigFileUrl: "https://drive.google.com/uc?export=view&id=1ys0KJ200qUiralOGfHWYL6dRIaXGPLf3"}
 	db.Create(&art4)
 	DB = db
+}
+
+func GetArtForId(ArtId uint) (art Art, err error) {
+	err = DB.First(&art, ArtId).Error
+	return art, err
 }
 
 func GetOrderForId(OrderId uint) (order Order, err error) {
@@ -52,7 +58,20 @@ func GetOrderForId(OrderId uint) (order Order, err error) {
 	return order, err
 }
 
-func GetArtForId(ArtId uint) (art Art, err error) {
-	err = DB.First(&art, ArtId).Error
-	return art, err
+func UpdateOrderToPaid(OrderId uint) (order Order, err error) {
+	err = DB.First(&order, OrderId).Error
+	if err != nil {
+		return order, err
+	}
+	DB.Model(&order).Update("paid", true)	
+	return order, err
+}
+
+func UpdateOrderToEmailSent(OrderId uint) (order Order, err error) {
+	err = DB.First(&order, OrderId).Error
+	if err != nil {
+		return order, err
+	}
+	DB.Model(&order).Update("email_sent", true)	
+	return order, err
 }
